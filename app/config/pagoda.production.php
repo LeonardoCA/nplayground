e7634def08e14436c66eed0016af21013ec33c3b
Leonard Odlozilik <leonard.odlozilik@gmail.com>
2015-01-18 01:28:26 +0100

    feature: pagoda cloud config

---
+++ app/config/pagoda.production.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'parameters' => [
+        'database' => [
+            'driver' => 'mysql',
+            'host' => getenv('DATABASE1_HOST'),
+            'port' => getenv('DATABASE1_PORT'),
+            'dbname' => getenv('DATABASE1_NAME'),
+            'user' => getenv('DATABASE1_USER'),
+            'password' => getenv('DATABASE1_PASS'),
+        ],
+    ],
+];
